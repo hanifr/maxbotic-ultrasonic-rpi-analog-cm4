6394e4a70efbdeeb8de94a483c0564446cf23f26
hanifr <irhaniframli@gmail.com>
2025-07-20 12:39:31 +0800

    Update mqtt_service.sh

--- mqtt_service.sh
+++ mqtt_service.sh
@@ -5,8 +5,8 @@
 # Note: Update these values according to your setup
 
 # MQTT Broker Configuration
-export MQTT_BROKER="${MQTT_BROKER:-zr.txio.live}"
-export MQTT_PORT="${MQTT_PORT:-1880}"
+export MQTT_BROKER="${MQTT_BROKER:-xx.xxx.xxx}"
+export MQTT_PORT="${MQTT_PORT:-1883}"
 export MQTT_TOPIC="${MQTT_TOPIC:-dtonggang/ultrasonic-01}"
 export MQTT_CLIENT_ID="${MQTT_CLIENT_ID:-cm4-1}"
 
